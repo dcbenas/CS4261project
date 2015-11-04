4b0912646a42dfded74487fd72767a0371a880f6
Shehmeer Jiwani <shehmeer.jiwani@gmail.com>
2015-11-04 04:41:09 -0500

    filtering out cleaners

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,13 +9,15 @@ class OrdersController < ApplicationController
 		result.merchants.each do |m|
 			puts m
 			puts "\n\n\n"
-			results_hash[m.id] = {
-				name: m.summary.name,
-				address: "#{m.location.street}, #{m.location.city}, #{m.location.state}",
-				distance: m.location.distance,
-				#TODO: Fix cuisines, returns null always
-				cuisine: m.cuisines
-			}
+			if m.summary.type_label == "Restaurant"
+				results_hash[m.id] = {
+					name: m.summary.name,
+					address: "#{m.location.street}, #{m.location.city}, #{m.location.state}",
+					distance: m.location.distance,
+					#TODO: Fix cuisines, returns null always
+					cuisine: m.cuisines
+				}
+			end
 		end
 
 		render json: results_hash
